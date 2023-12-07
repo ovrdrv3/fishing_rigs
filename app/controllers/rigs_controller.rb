3c3eb5b8b0f441a5e65ef334fe3482816f6f84db
Donaven Snowden <donaven.snowden@gmail.com>
2023-12-07 01:34:03 -0800

    Add React on Rails scaffolding

--- app/controllers/rigs_controller.rb
+++ app/controllers/rigs_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class RigsController < ApplicationController
-  def index; end
+  layout 'hello_world'
+
+  def index
+    @hello_world_props = { name: 'Stranger' }
+  end
 end

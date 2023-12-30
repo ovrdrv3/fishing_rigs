228d9c709d8aefcda5d484f23349a31e437f841c
Donaven Snowden <donaven.snowden@gmail.com>
2023-12-29 16:32:28 -0800

    Add style components, update mobile look

--- app/javascript/components/Sidebar.jsx
+++ app/javascript/components/Sidebar.jsx
@@ -10,6 +10,8 @@ import {
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import Logo from "./Logo";
+import Favicon from "../../assets/images/favicon.svg";
+import styled from "styled-components";
 
 const navigation = [
   {
@@ -38,24 +40,61 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
+const MobileOnlyDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  @media screen and (min-width: 640px) {
+    display: none;
+  }
+`;
+
+function SidebarItem({ item, current_url }) {
+  return (
+    <li key={item.name}>
+      <div
+        className={classNames(
+          item.href == current_url
+            ? "bg-gray-400 dark:bg-gray-800 text-white"
+            : "text-gray-400 hover:text-white hover:bg-gray-400 dark:hover:bg-gray-800",
+          "flex group justify-between gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
+        )}
+      >
+        <a href={item.href} className="flex gap-x-3 w-full">
+          <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+          {item.name}
+        </a>
+        <a href={`${item.href}/new`} className="flex-none">
+          <PlusIcon className="h-6 w-6 shrink-0" />
+        </a>
+      </div>
+    </li>
+  );
+}
+
 export default function Sidebar({ current_url }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <>
-      <button
-        type="button"
-        className="-m-2.5 p-2.5 text-white xl:hidden"
-        onClick={() => setSidebarOpen(true)}
-      >
-        <span className="sr-only">Open sidebar</span>
-        <Bars3Icon
-          className="h-8 w-8"
-          aria-hidden="true"
-          style={{ height: "24px", width: "24px" }}
-        />
-      </button>
-
+      <div className="flex justify-between dark:invert">
+        <MobileOnlyDiv className="xl:hidden">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-300 dark:text-white"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon className="h-8 w-8" aria-hidden="true" />
+          </button>
+        </MobileOnlyDiv>
+        <MobileOnlyDiv className="xl:hidden">R I G S</MobileOnlyDiv>
+        <MobileOnlyDiv className="xl:hidden">
+          <img className="p-1 h-8 w-8" src={Favicon} alt="Logo" />
+        </MobileOnlyDiv>
+      </div>
       <Transition.Root show={sidebarOpen} as={Fragment}>
         <Dialog
           as="div"
@@ -109,31 +148,17 @@ export default function Sidebar({ current_url }) {
                   </div>
                 </Transition.Child>
                 <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 ring-1 ring-white/10">
-                  <div className="flex h-16 shrink-0 items-center">
+                  <div className="flex shrink-0 items-center justify-center">
                     <Logo />
                   </div>
                   <nav className="flex flex-1 flex-col">
                     <ul role="list" className="flex flex-1 flex-col gap-y-7">
                       {navigation.map((item) => (
-                        <li key={item.name}>
-                          <a
-                            href={item.href}
-                            className={classNames(
-                              item.current
-                                ? "bg-gray-400 dark:bg-gray-800 text-white"
-                                : "text-gray-400 hover:text-white hover:bg-gray-400 dark:hover:bg-gray-800",
-                              "flex-1 group flex justify-between gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                            )}
-                          >
-                            <div className="flex gap-x-3">
-                              <item.icon
-                                className="h-6 w-6 shrink-0"
-                                aria-hidden="true"
-                              />
-                              {item.name}
-                            </div>
-                          </a>
-                        </li>
+                        <SidebarItem
+                          key={item.name}
+                          item={item}
+                          current_url={current_url}
+                        />
                       ))}
                     </ul>
                   </nav>
@@ -150,27 +175,11 @@ export default function Sidebar({ current_url }) {
           <nav className="flex flex-1 flex-col">
             <ul role="list" className="flex flex-1 flex-col gap-y-4">
               {navigation.map((item) => (
-                <li key={item.name}>
-                  <div
-                    className={classNames(
-                      item.href == current_url
-                        ? "bg-gray-400 dark:bg-gray-800 text-white"
-                        : "text-gray-400 hover:text-white hover:bg-gray-400 dark:hover:bg-gray-800",
-                      "flex group justify-between gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-                    )}
-                  >
-                    <a href={item.href} className="flex gap-x-3">
-                      <item.icon
-                        className="h-6 w-6 shrink-0"
-                        aria-hidden="true"
-                      />
-                      {item.name}
-                    </a>
-                    <a href={`${item.href}/new`} className="flex-none">
-                      <PlusIcon className="h-6 w-6 shrink-0" />
-                    </a>
-                  </div>
-                </li>
+                <SidebarItem
+                  key={item.name}
+                  item={item}
+                  current_url={current_url}
+                />
               ))}
             </ul>
           </nav>

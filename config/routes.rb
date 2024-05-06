b6b06cf06d804865646f1c1e2aa012da0203c3cd
Donaven Snowden <donaven.snowden@gmail.com>
2024-05-05 23:34:31 -0700

    Remove hello world from routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :hooks
-  get 'hello_world', to: 'hello_world#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
